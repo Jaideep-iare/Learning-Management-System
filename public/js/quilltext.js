1bc13da09813e8c8bb4ee699428db2baa4f394ce
Jaideep-iare <21951a0567@iare.ac.in>
2024-09-25 00:01:21 +0530

    Enhance functionalities of text editor

---
+++ public/js/quilltext.js
@@ -0,0 +1,23 @@
+// Initialize Quill editor
+var quill = new Quill('#editor-container', {
+    theme: 'snow',  // Specify theme
+    modules: {
+      toolbar: [
+        [{ 'bold': true }, { 'italic': true }, { 'underline': true }], // Formatting options
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],
+        [{ 'indent': '-1'}, { 'indent': '+1' }],
+        [{ 'direction': 'rtl' }],
+        [{ 'size': ['small', false, 'large', 'huge'] }],
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        [{ 'color': [] }, { 'background': [] }], // Dropdown with defaults from theme
+        [{ 'align': [] }],
+      ]
+    }
+  });
+
+  // On form submit, transfer content to hidden input
+  document.getElementById('text-form').onsubmit = function() {
+    var htmlContent = quill.root.innerHTML;
+    document.getElementById('formattedText').value = htmlContent;
+  };
