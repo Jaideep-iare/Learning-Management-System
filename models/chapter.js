bef5e4ffcaf5d4ceee4e1dcb073608bdb146fd73
Jaideep-iare <21951a0567@iare.ac.in>
2024-09-25 23:25:05 +0530

    Add all the necessary models

--- models/chapter.js
+++ models/chapter.js
@@ -3,36 +3,22 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Chapter extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-
     // static associate(models) {
     //   // define association here
     // }
-
   }
-  User.init({
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    role: DataTypes.STRING
+  Chapter.init({
+    chaptername: DataTypes.STRING,
+    chapterdescription: DataTypes.TEXT,
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'Chapter',
   });
-  return User;
+  return Chapter;
 };
--- models/report.js
+++ models/report.js
@@ -3,36 +3,22 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Report extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-
     // static associate(models) {
     //   // define association here
     // }
-
   }
-  User.init({
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    role: DataTypes.STRING
+  Report.init({
+    totalstudents: DataTypes.INTEGER,
+    completedstudents: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'Report',
   });
-  return User;
+  return Report;
 };
