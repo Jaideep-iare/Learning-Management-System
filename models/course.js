72d2453f9c7c9fad30124d3452c4183cb6832071
Jaideep-iare <21951a0567@iare.ac.in>
2024-09-28 23:33:29 +0530

    make faculty alias to fetch faculty name

--- models/course.js
+++ models/course.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
       });
       Course.belongsTo(models.User, {
         foreignKey: "facultyid",
+        as: 'faculty', // Alias for User model
       });
       Course.hasMany(models.Enrollment, {
         foreignKey: "courseid",
@@ -22,14 +23,50 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: "courseid",
       });
     }
+
     static findAvailableCourse() {
-      return this.findAll();
-    }
+      return this.findAll({
+         include: {
+            model: sequelize.models.User, // Include User (faculty) model
+            as: 'faculty', // The alias we defined earlier
+            attributes: ['name'] // Only include faculty's name
+         }
+      });
+   }
+   
+
+    static async findFacultyCourse(loggedInUser) {
+      try {
+          return await this.findAll({
+              where: {
+                  facultyid: loggedInUser.id
+              }
+          }) || []; // If no records, return an empty array
+      } catch (error) {
+          console.error("Error fetching faculty courses:", error);
+          return [];
+      }
+  }
+  
   }
   Course.init(
     {
-      coursename: DataTypes.STRING,
-      description: DataTypes.TEXT,
+      coursename: {
+        type : DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false, 
+      },
+      facultyid:{
+        type: DataTypes.INTEGER,
+      allowNull: false, // Make sure that each course is assigned to a faculty
+      references: {
+        model: 'Users', // Assumes that the user model is stored in the 'Users' table
+        key: 'id',
+      },
+      }
     },
     {
       sequelize,
