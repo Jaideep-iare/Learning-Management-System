072d06336210dd6391f546d55f5045c00ad0a9c0
Jaideep-iare <21951a0567@iare.ac.in>
2024-10-06 15:56:09 +0530

    Check for view report on render

--- app.js
+++ app.js
@@ -567,6 +567,7 @@ app.get(
     try {
       // Get the logged-in faculty user
       const facultyId = req.user.id;
+    
 
       // Fetch all courses created by this faculty
       const facultyCourses = await Course.findAll({
@@ -595,37 +596,22 @@ app.get(
         const totalPages = await Page.getPagesCountByChapterIds(chapterIds);
 
         // Get the number of students who have completed the course (i.e., completed all pages)
-        // const completedStudents = await sequelize.query(
-        //   `
-        //     SELECT e.studentid
-        //     FROM public."Enrollments" e
-        //     JOIN public."Progresses" p ON p.studentid = e.studentid
-        //     WHERE e.courseid = :courseId
-        //     AND p.iscompleted = true
-        //     GROUP BY e.studentid
-        //     HAVING COUNT(p.pageid) = :totalPages
-        // `,
-        //   {
-        //     replacements: { courseId: course.id, totalPages: totalPages },  //replacements are done to protect from sql query attacks
-        //     type: sequelize.QueryTypes.SELECT,
-        //   }
-        // );
         const completedStudents = await sequelize.query(
           `
             SELECT e.studentid
-            FROM enrollments e
-            JOIN progresses p ON p.studentid = e.studentid
+            FROM public."Enrollments" e
+            JOIN public."Progresses" p ON p.studentid = e.studentid
             WHERE e.courseid = :courseId
             AND p.iscompleted = true
             GROUP BY e.studentid
             HAVING COUNT(p.pageid) = :totalPages
-          `,
+        `,
           {
-            replacements: { courseId: course.id, totalPages: totalPages },
+            replacements: { courseId: course.id, totalPages: totalPages },  //replacements are done to protect from sql query attacks
             type: sequelize.QueryTypes.SELECT,
-            logging: console.log,  // This will log the query to the console
           }
         );
+
         // Get the number of students who are currently progressing (not completed)
         const inProgressStudents = totalEnrollments - completedStudents.length;
 
